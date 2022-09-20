10f54eec17f5770ce0b9e6335d1b1c85a701f25d
diode231 <mattspenn@mac.com>
2022-09-20 12:18:57 -0500

    Initial Commit

---
+++ Netflix Clone/Controllers/DownloadsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DownloadsViewController.swift
+//  Netflix Clone
+//
+//  Created by Matt Spenn on 9/19/22.
+//
+
+import UIKit
+
+class DownloadsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+}
+
---
+++ Netflix Clone/Controllers/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  Netflix Clone
+//
+//  Created by Matt Spenn on 9/19/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+
+}
